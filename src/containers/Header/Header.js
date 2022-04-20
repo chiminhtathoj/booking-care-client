1007663dbfe0fe97fb0e6a56956049849d3517d3
chiminhtathoj <vdlong2009@gmail.com>
2022-04-20 23:06:13 +0700

    design header admin

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -5,29 +5,59 @@ import * as actions from "../../store/actions";
 import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
+import { LANGUAGES } from "../../utils/constant"
 
-class Header extends Component {
 
-    render() {
-        const { processLogout } = this.props;
-
-        return (
-            <div className="header-container">
-                {/* thanh navigator */}
-                <div className="header-tabs-container">
-                    <Navigator menus={adminMenu} />
-                </div>
-
-                {/* nút logout */}
-                <div className="btn btn-logout" onClick={processLogout}>
+function Header(props) {
+    const { processLogout } = props
+    const handleChangeLanguage = (language) => {
+        alert(language)
+    }
+    return (
+        <div className="header-container">
+            {/* thanh navigator */}
+            <div className="header-tabs-container">
+                <Navigator menus={adminMenu} />
+            </div>
+            <div className="header-languages">
+                <span className="language-vi" onClick={() => handleChangeLanguage(LANGUAGES.VI)}>VN</span>
+                <span className="language-en" onClick={() => handleChangeLanguage(LANGUAGES.EN)}>EN</span>
+                <div className="btn btn-logout" onClick={processLogout} title="log out">
                     <i className="fas fa-sign-out-alt"></i>
                 </div>
             </div>
-        );
-    }
 
+            {/* nút logout */}
+
+        </div>
+    );
 }
 
+// class Header extends Component {
+//     const { processLogout } = this.props
+// render() {
+//     const { processLogout } = this.props;
+
+//     return (
+//         <div className="header-container">
+//             {/* thanh navigator */}
+//             <div className="header-tabs-container">
+//                 <Navigator menus={adminMenu} />
+//             </div>
+//             <div className="header-languages">
+//                 <span className="language-vi" onClick={() => handleChangeLanguage(LANGUAGES.VI)}>VN</span>
+//                 <span className="language-en" onClick={() => handleChangeLanguage(LANGUAGES.EN)}>EN</span>
+//                 <div className="btn btn-logout" onClick={processLogout} title="log out">
+//                     <i className="fas fa-sign-out-alt"></i>
+//                 </div>
+//             </div>
+
+//             {/* nút logout */}
+
+//         </div>
+//     )
+// }
+// }
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn
